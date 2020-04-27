3f89a8dbac0a5384fae8d4c1a0d22af3d5391d8b
devil1994 <domendra2014@gmail.com>
2020-04-27 16:22:14 +0530

    File Download api added

--- src/main/java/com/domen/controller/Controller.java
+++ src/main/java/com/domen/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.domen.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.LocalTime;
@@ -10,6 +11,8 @@ import com.domen.entity.FileDetail;
 import com.domen.service.FileService;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -95,6 +98,26 @@ public static String fileDirectory = System.getProperty("user.dir") + "/uploaded
 
 		return new ResponseEntity<>(message, HttpStatus.OK);
 	}
+
+    @RequestMapping(value = "/download", method = RequestMethod.GET)
+    public ResponseEntity<Object> downloadFile(@RequestParam("path") String path,@RequestParam("filename") String filename) throws IOException
+    {
+        String downloadableFile =path+"/"+filename ;
+        File file = new File(downloadableFile);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition",
+                String.format("attachment; filename=\"%s\"", file.getName()));
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        ResponseEntity<Object> responseEntity = ResponseEntity.ok().headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/txt")).body(resource);
+
+        return responseEntity;
+    }
 
 	@GetMapping("getUploadedFileDetailsByUsername")
 	@ApiOperation(value = "Uploaded file details of user")
