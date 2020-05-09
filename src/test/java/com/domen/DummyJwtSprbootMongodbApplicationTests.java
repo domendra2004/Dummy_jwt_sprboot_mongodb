b992263e49270a74887c150a604d65fab6cbb4e2
devil1994 <domendra2014@gmail.com>
2020-05-10 03:47:50 +0530

    Vulnerabilities,Bugs and Security Hotspots removed.

--- src/test/java/com/domen/DummyJwtSprbootMongodbApplicationTests.java
+++ src/test/java/com/domen/DummyJwtSprbootMongodbApplicationTests.java
@@ -1,14 +1,14 @@
 package com.domen;
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DummyJwtSprbootMongodbApplicationTests {
 
-
 	@Test
 	void contextLoads(){
 
-			}
+	}
 
 }
